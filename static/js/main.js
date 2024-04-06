7858985ce0af360c0714b6cb1cec115680ea63bc
ripa01 <ripabiswas2739@gmail.com>
2024-04-06 15:37:41 +0600

    update base & home view
    update some functionality in base and add requirements.txt file.

--- static/js/main.js
+++ static/js/main.js
@@ -62,79 +62,55 @@ $(document).ready(function () {
 });
 
 
-// disabling inspect element
-// document.addEventListener("contextmenu", function (e) {
-//     e.preventDefault(); //this prevents right click
-// });
-// document.onkeydown = function (e) {
-//     if (event.keycode == 123) {
-//         return false;
-//     }
-//     if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
-//         return false;
-//     }
-//     if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
-//         return false;
-//     }
-//     if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
-//         return false;
-//     }
-//     if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
-//         return false;
-//     }
-//     if (e.ctrlKey && e.keyCode == "S".charCodeAt(0)) {
-//         return false;
-//     }
-// };
 
 //step 1: get DOM
-let nextDom = document.getElementById('next');
-let prevDom = document.getElementById('prev');
+// let nextDom = document.getElementById('next');
+// let prevDom = document.getElementById('prev');
 
-let carouselDom = document.querySelector('.carousel');
-let SliderDom = carouselDom.querySelector('.carousel .list');
-let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
-let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
-let timeDom = document.querySelector('.carousel .time');
+// let carouselDom = document.querySelector('.carousel');
+// let SliderDom = carouselDom.querySelector('.carousel .list');
+// let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
+// let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
+// let timeDom = document.querySelector('.carousel .time');
 
-thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-let timeRunning = 3000;
-let timeAutoNext = 7000;
+// thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+// let timeRunning = 3000;
+// let timeAutoNext = 7000;
 
-nextDom.onclick = function(){
-    showSlider('next');    
-}
+// nextDom.onclick = function(){
+//     showSlider('next');    
+// }
 
-prevDom.onclick = function(){
-    showSlider('prev');    
-}
-let runTimeOut;
-let runNextAuto = setTimeout(() => {
-    next.click();
-}, timeAutoNext)
-function showSlider(type){
-    let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
-    let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
+// prevDom.onclick = function(){
+//     showSlider('prev');    
+// }
+// let runTimeOut;
+// let runNextAuto = setTimeout(() => {
+//     next.click();
+// }, timeAutoNext)
+// function showSlider(type){
+//     let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+//     let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
     
-    if(type === 'next'){
-        SliderDom.appendChild(SliderItemsDom[0]);
-        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-        carouselDom.classList.add('next');
-    }else{
-        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
-        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
-        carouselDom.classList.add('prev');
-    }
-    clearTimeout(runTimeOut);
-    runTimeOut = setTimeout(() => {
-        carouselDom.classList.remove('next');
-        carouselDom.classList.remove('prev');
-    }, timeRunning);
+//     if(type === 'next'){
+//         SliderDom.appendChild(SliderItemsDom[0]);
+//         thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+//         carouselDom.classList.add('next');
+//     }else{
+//         SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+//         thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+//         carouselDom.classList.add('prev');
+//     }
+//     clearTimeout(runTimeOut);
+//     runTimeOut = setTimeout(() => {
+//         carouselDom.classList.remove('next');
+//         carouselDom.classList.remove('prev');
+//     }, timeRunning);
 
-    clearTimeout(runNextAuto);
-    runNextAuto = setTimeout(() => {
-        next.click();
-    }, timeAutoNext)
-}
+//     clearTimeout(runNextAuto);
+//     runNextAuto = setTimeout(() => {
+//         next.click();
+//     }, timeAutoNext)
+// }
 
 
