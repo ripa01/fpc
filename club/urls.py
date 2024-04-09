f80dff6de8aea8278acb658eb39c9246ea344da1
ripa01 <ripabiswas2739@gmail.com>
2024-04-09 23:54:42 +0600

    Added committee url in urls.py and base html

--- club/urls.py
+++ club/urls.py
@@ -9,19 +9,22 @@ urlpatterns =  [
     path('logout/', logout_view, name='logout'),
     path('about/', about_view, name='about'),
 
-    
+    # NEWS URLS
     path('news/', NewsListView.as_view(), name='news'),
     path('news/<int:pk>/', NewsDetailView.as_view(), name='news_details'),
     path('news/new/', NewsCreateView.as_view(), name='news_create'),
     path('news/<int:pk>/update/', NewsUpdateView.as_view(), name='news_update'),
     path('news/<int:pk>/delete/', NewsDeleteView.as_view(), name='news_delete'),
 
-
+    # NOTICE URLS
     path('notice/', NoticeListView.as_view(), name='notice'),
     path('notice/<int:pk>/', NoticeDetailView.as_view(), name='notice_details'),
     path('notice/notice/', NoticeCreateView.as_view(), name='notice_create'),
-    path('notice/<int:pk>/update/', NoticeUpdateView.as_view(), name='notice_update'),
-    path('notice/<int:pk>/delete/', NoticeDeleteView.as_view(), name='notice_delete'),
+    
+
+    #Committee URLS
+    path('committee/', CommitteeListView.as_view(), name='committee'),
+
 ]
 
 # Serve media files during development
